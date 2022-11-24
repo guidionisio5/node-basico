2067b33b83f8aeb0fdba113e53b1b79f9bfd0a3c
guidionisio5 <101677648+guidionisio5@users.noreply.github.com>
2022-11-23 22:28:16 -0300

    atividade5 usando modulo

---
+++ aula01/atividade05.js
@@ -0,0 +1,4 @@
+// importa o modulo de calculadora
+const calculadora = require("./modulos/calculadora");
+
+console.log(calculadora.soma(10,20))
---
+++ aula01/modulos/calculadora.js
@@ -0,0 +1,8 @@
+// cria e exporta o modulo calculadora
+module.exports = {
+    soma:(a,b)=>parseFloat(a)+parseFloat(b),
+    sub:(a,b)=>parseFloat(a)-parseFloat(b),
+    mult:(a,b)=>parseFloat(a)*parseFloat(b),
+    div:(a,b)=>parseFloat(a)/parseFloat(b)
+}
+
