fd77ed8c69642061b219e8c3a7e7c1b0228474e6
guidionisio5 <101677648+guidionisio5@users.noreply.github.com>
2022-11-30 22:24:23 -0300

    rota post inicio

--- aula04/at12-renderizarHTML/index.js
+++ aula04/at12-renderizarHTML/index.js
@@ -10,6 +10,16 @@ const porta = 3660
 // define a pasta public com o conteudo static (CSS,JS,IMG)
 app.use(express.static('views/public'))
 
+// middleware
+// rota que toda requisicao possa ser ela
+// rota padrao
+
+// decoficando os parametros enviados para a rota
+app.use(express.urlencoded({extended: true}))
+
+// converte os valores para formato JSON 
+app.use(express.json())
+
 // cria a rota
 app.get('/',(req,res)=>{
     res.status(200)
@@ -21,6 +31,21 @@ app.get('/cadastrar',(req,res)=>{
     res.status(200)
     res.sendFile(__dirname+'/views/cadastrar.html')
 })
+
+// cri a rota para cadastrar login
+app.post('/cadastrar/login',(req,res)=>{
+    // res.send(req.body)
+    // desestruturação de dados
+
+    // desestruturacao basica campo a campo
+    // let nome = req.body.nome
+    
+    // desestruturacao de varios campos
+    let {nome,email,senha,confirmar} = req.body
+
+    res.send(`Nome: ${nome} Email: ${email} Senha: ${senha} Confirmar: ${confirmar}`)
+
+}) 
 
 // cria a rota consultar
 app.get('/consultar',(req,res)=>{
