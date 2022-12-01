a8930dcc28f878d302a6efc401eac6501ec53f0a
guidionisio5 <101677648+guidionisio5@users.noreply.github.com>
2022-11-30 21:50:57 -0300

    view cadastrar login

--- aula04/at12-renderizarHTML/index.js
+++ aula04/at12-renderizarHTML/index.js
@@ -7,6 +7,9 @@ const app = express()
 // porta do servidor 
 const porta = 3660
 
+// define a pasta public com o conteudo static (CSS,JS,IMG)
+app.use(express.static('views/public'))
+
 // cria a rota
 app.get('/',(req,res)=>{
     res.status(200)
@@ -16,14 +19,16 @@ app.get('/',(req,res)=>{
 // cria a rota cadastrar
 app.get('/cadastrar',(req,res)=>{
     res.status(200)
-    res.send('<h1>Cadastar - Rotas</h1>')
+    res.sendFile(__dirname+'/views/cadastrar.html')
 })
 
 // cria a rota consultar
 app.get('/consultar',(req,res)=>{
     res.status(200)
-    res.send('<h1>Consultar - Rotas</h1>')
+    res.send('<h1>Consultar</h1>')
 })
+
+
 
 // rota para retorno da pagina de erro
 // super mega importante -> ficar no final das rotas
